7ebc10b6443f6934f5e82bc8226c78f9b5336a42
Zach Sloane <zacharslo@gmail.com>
2015-02-02 21:11:52 -0500

    A huge huge start.
    -wordList class is started including quickSort
    -grid class (the matrix stuff confused me a bit so that won’t be active
    yet but that’s in progress)
    -findMatches()
    -We’re going to need to add a matrix file to be written to, and a file
    to handle exceptions and errors
    
    Try to compile this though, I’m only getting one error, and I’m not
    sure why that error is showing up.

---
+++ Project 2A/Word Search/Word Search/grid.h
@@ -0,0 +1,31 @@
+/*
+Grid.h
+Author: Zach Sloane and Andrew Blum
+Description: Used to read a matrix of words from file to search through
+
+*/
+
+#include <stdlib.h>
+#include <string>
+#include <stdexcept>
+#include <fstream>
+#include <iostream>
+
+//#include "d_matrix.h" this is going to be needed
+#include "wordList.h"
+
+using namespace std;
+
+class grid
+{
+	public: 
+		grid();
+    grid(const string& name);
+    vector<string> findMatches(wordList& wordl) const;
+		
+
+	private:
+		void readGrid(const string& name);
+		void parseLine(const string& line, int lineNum);
+};
+
